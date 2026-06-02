d03d063c83533aeecca3bb691ad51f0189fdb7b4
peak-like <2095991530@qq.com>
2026-06-03 00:22:41 +0800

    feat: 提升boot版本提高spring ai兼容性+引入zhipuai+webflux

---
+++ alumni-direct-service/src/main/java/com/lightcs/controller/AiInterviewController.java
@@ -0,0 +1,83 @@
+package com.lightcs.controller;
+
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.zhipuai.ZhiPuAiChatModel;
+import org.springframework.ai.zhipuai.ZhiPuAiChatOptions;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AiInterviewController {
+
+    private final ChatClient chatClient;
+
+    @Resource
+    private ZhiPuAiChatModel chatModel;
+
+    public AiInterviewController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/ai")
+    String generation(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+    }
+
+    /**
+     * glm 模型直接调用
+     *
+     * @return
+     */
+    @GetMapping("/call/chat")
+    public String callChat(@RequestParam String message, @RequestParam(defaultValue = "glm-4.6") String model) {
+        ChatResponse response = chatModel.call(
+                new Prompt(
+                        message,
+                        ZhiPuAiChatOptions.builder()
+                                .model(model)
+                                .temperature(0.7)
+                                .build()
+                ));
+        return response.getResult().getOutput().getText();
+    }
+
+    /**
+     * glm 模型流式调用
+     *
+     * @return
+     */
+    @GetMapping(value = "/stream/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent<Object>> streamChat(@RequestParam String message, @RequestParam(defaultValue = "glm-4.6") String model) {
+        return chatModel.stream(
+                        new Prompt(message, ZhiPuAiChatOptions.builder().model(model).build())
+                )
+                .map(chatResponse -> {
+                    String text = chatResponse.getResults()
+                            .stream()
+                            .filter(r -> r.getOutput().getText() != null)
+                            .map(r -> r.getOutput().getText())
+                            .findFirst()
+                            .orElse("");
+                    return ServerSentEvent.builder()
+                            .data(text)
+                            .build();
+                })
+                .concatWith(Flux.just(
+                        ServerSentEvent.builder()
+                                .data("[DONE]")
+                                .build()
+                ))
+                .doOnError(Throwable::printStackTrace);
+    }
+}
